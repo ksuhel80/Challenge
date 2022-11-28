26e6892f89821a6cb90de4f564574bc3df8d8517
ksuhel80 <ksuhel80@gmail.com>
2022-11-29 01:45:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -152,7 +152,9 @@ app.get("/", async (req, res) => {
 
 app.get("/sectors", async (req, res) => {
   const Product = mongoose.model("sectors", productSchema);
-  let data = await Product.find({}).SetFields(Fields.Exclude("_id"));
+  let data = await Product.find({}).SetFields(
+    Fields.Include("label", "children").Exclude("_id")
+  );
   console.log(data);
 
   res.send(data);
